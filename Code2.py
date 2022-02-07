05acadbb997c57df7ce223af6c664333363d037b
luiz.braz@live.com <luiz.braz@live.com>
2022-02-07 22:04:43 +0000

    Add Feature

--- Code2.py
+++ Code2.py
@@ -2,7 +2,3 @@
 def generate_data2(table_name="my_data"):
   df = spark.range(0,10)
   df.write.format("delta").mode("overwrite").saveAsTable(table_name)
-
-# COMMAND ----------
-
-# another release trigger one more change
